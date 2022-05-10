3ee72b2e598e8d5c53dfa915cb9fef1a2a81fcdd
yvban1412 <66365725+yvban1412@users.noreply.github.com>
2022-05-10 21:30:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'SHEN CHEN BLOG',
+  author: 'Shen Chen',
+  email: 'mikuai@qq.com',
+  link: 'https://zcx.cool',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
